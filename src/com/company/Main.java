a2304db3b6f75a37324264ca5fcf50d5b8c48e9b
Overlord7667 <aspiaro@gmail.com>
2021-11-01 19:22:27 +0300

    Array

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,26 +1,20 @@
 package com.company;
-import java.lang.String;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-		//for (int i = 100; i > 10; i -= 10)
-		//	System.out.println(i);
-
-		boolean isTrue = true;
-		int j = 0;
-		while (j < 100) {
-			j += 1;
-			if (j % 2 == 0)
-				continue;
-				System.out.println(j);
-
-		}
-//		int x = 13;
-//		do {
-//			x--;
-//			System.out.println(x);
-//		}while (x > 10);
-	}
+    public static void main(String[] args)
+    {
+    int[] a;
+	int n;
+	Scanner in = new Scanner(System.in);
+	System.out.print("Enter numbers of array: ");
+	n = in.nextInt();
+	a = new int[n];
+	for (int i = 0; i < n; i++ )
+    {
+        System.out.print("Enter a[" + i +"] = ");
+        a[i] = in.nextInt();
+    }
+    }
 }
